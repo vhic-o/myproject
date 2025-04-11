da00533659c56c4d37696b610f422820de081548
vhic-o <onehivictoria01@gmail.com>
2025-04-11 14:38:21 +0100

    updated project

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
-//get the div  for the ham hurget menu
-const HamburgerMenu = document.getElementById("hamB");
-//event lisntern to  notify when is clicked
-HamburgerMenu.onclick= function togglemenu(){
-    const nav = document.getElementById("navLinks");
-  nav.classList.toggle("show");
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
